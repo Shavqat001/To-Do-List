ffdfb848eb66dc37f9ace47677c47dabb3d280d2
Shavqat001 <nazriev.shavqatjon@gmail.com>
2023-08-12 19:10:52 +0500

    v 3.3.0

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ function addLiToUl() {
       });
    } else {
       field.style.outlineColor = 'tomato';
+      alert('You must write something');
    }
    field.focus();
    field.value = '';
@@ -61,46 +62,59 @@ function addLiToUl() {
 
 function saveData() {
    let arrItems = [];
+   let arrClasses = [];
 
    let items = document.querySelectorAll('.item');
+   let text = document.querySelectorAll('span');
+
    for (let i = 0; i < items.length; i++) {
       arrItems.push(items[i].textContent);
    }
 
+   text.forEach(el => {
+      for (let i = 0; i < el.classList.length; i++) {
+         arrClasses.push(el.classList[i]);
+      }
+   });
+
    localStorage.setItem('items', JSON.stringify(arrItems));
+   localStorage.setItem('classes', JSON.stringify(arrClasses));
 }
 
 function loadData() {
    let arrItems = JSON.parse(localStorage.getItem('items'));
-   if (arrItems) {
-      for (let i = 0; i < arrItems.length; i++) {
-         let liEl = document.createElement('li'),
-            text = document.createElement('span'),
-            div = document.createElement('div'),
-            editBtn = document.createElement('button'),
-            delBtn = document.createElement('button');
+   let arrClasses = JSON.parse(localStorage.getItem('classes'));
 
-         liEl.classList.add('item');
-         text.textContent = arrItems[i];
-         editBtn.classList.add('edit-btn');
-         delBtn.classList.add('del-btn');
+   for (let i = 0; i < arrItems.length; i++) {
+      let liEl = document.createElement('li'),
+         text = document.createElement('span'),
+         div = document.createElement('div'),
+         editBtn = document.createElement('button'),
+         delBtn = document.createElement('button');
 
-         div.append(editBtn);
-         div.append(delBtn);
-         liEl.prepend(text);
-         liEl.append(div);
-         listEl.append(liEl);
+      liEl.classList.add('item');
+      text.textContent = arrItems[i];
+      text.classList.add(arrClasses[i]);
+      editBtn.classList.add('edit-btn');
+      delBtn.classList.add('del-btn');
+                   
+      div.append(editBtn);
+      div.append(delBtn);
+      liEl.prepend(text);
+      liEl.append(div);
+      listEl.append(liEl);
 
-         text.addEventListener('click', () => {
-            text.classList.toggle('done');
-         });
+      text.addEventListener('click', () => {
+         text.classList.toggle('done');
+         saveData();
+      });
 
-         editBtn.addEventListener('click', () => {
-         });
+      editBtn.addEventListener('click', () => {
+      });
 
-         delBtn.addEventListener('click', () => {
-            liEl.remove();
-         });
-      }
+      delBtn.addEventListener('click', () => {
+         liEl.remove();
+         saveData();
+      });
    }
 }

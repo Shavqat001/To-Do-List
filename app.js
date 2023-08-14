bf42b3fadc819bac388f5239e4018960aea7dc16
Shavqat001 <nazriev.shavqatjon@gmail.com>
2023-08-14 19:36:59 +0500

    fixing some issues

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ let formEL = document.querySelector('form'),
 
 formEL.addEventListener('submit', (evt) => {
    evt.preventDefault();
-})
+   // Code to handle form submission
+});
 
 addBtn.addEventListener('click', () => {
    addLiToUl();
-})
+});
 
 window.addEventListener('load', () => {
-   field.focus();
    field.placeholder = 'write something . . .';
    loadData();
+   field.focus();
 });
 
 function addLiToUl() {
@@ -29,6 +30,7 @@ function addLiToUl() {
 
       liEl.classList.add('item');
       text.textContent = field.value;
+      text.classList.add('text');
       editBtn.classList.add('edit-btn');
       delBtn.classList.add('del-btn');
 
@@ -44,7 +46,7 @@ function addLiToUl() {
       });
 
       editBtn.addEventListener('click', () => {
-         saveData();
+         // Code to handle edit button click
       });
 
       delBtn.addEventListener('click', () => {
@@ -62,7 +64,7 @@ function addLiToUl() {
 
 function saveData() {
    let arrItems = [];
-   let arrClasses = [];
+   let arrDone = [];
 
    let items = document.querySelectorAll('.item');
    let text = document.querySelectorAll('span');
@@ -72,49 +74,53 @@ function saveData() {
    }
 
    text.forEach(el => {
-      for (let i = 0; i < el.classList.length; i++) {
-         arrClasses.push(el.classList[i]);
-      }
+      arrDone.push(el.classList.contains('done'));
    });
 
    localStorage.setItem('items', JSON.stringify(arrItems));
-   localStorage.setItem('classes', JSON.stringify(arrClasses));
+   localStorage.setItem('classes', JSON.stringify(arrDone));
 }
 
 function loadData() {
    let arrItems = JSON.parse(localStorage.getItem('items'));
    let arrClasses = JSON.parse(localStorage.getItem('classes'));
 
-   for (let i = 0; i < arrItems.length; i++) {
-      let liEl = document.createElement('li'),
-         text = document.createElement('span'),
-         div = document.createElement('div'),
-         editBtn = document.createElement('button'),
-         delBtn = document.createElement('button');
+   console.log(arrClasses);
+   if (arrItems && arrClasses) {
+      for (let i = 0; i < arrItems.length; i++) {
+         let liEl = document.createElement('li'),
+            text = document.createElement('span'),
+            div = document.createElement('div'),
+            editBtn = document.createElement('button'),
+            delBtn = document.createElement('button');
 
-      liEl.classList.add('item');
-      text.textContent = arrItems[i];
-      text.classList.add(arrClasses[i]);
-      editBtn.classList.add('edit-btn');
-      delBtn.classList.add('del-btn');
-                   
-      div.append(editBtn);
-      div.append(delBtn);
-      liEl.prepend(text);
-      liEl.append(div);
-      listEl.append(liEl);
+         liEl.classList.add('item');
+         text.textContent = arrItems[i];
+         if (arrClasses[i]) {
+            text.classList.add('done');
+         }
+         editBtn.classList.add('edit-btn');
+         delBtn.classList.add('del-btn');
 
-      text.addEventListener('click', () => {
-         text.classList.toggle('done');
-         saveData();
-      });
+         div.append(editBtn);
+         div.append(delBtn);
+         liEl.prepend(text);
+         liEl.append(div);
+         listEl.append(liEl);
 
-      editBtn.addEventListener('click', () => {
-      });
+         text.addEventListener('click', () => {
+            text.classList.toggle('done');
+            saveData();
+         });
 
-      delBtn.addEventListener('click', () => {
-         liEl.remove();
-         saveData();
-      });
+         editBtn.addEventListener('click', () => {
+            // Code to handle edit button click
+         });
+
+         delBtn.addEventListener('click', () => {
+            liEl.remove();
+            saveData();
+         });
+      }
    }
 }
